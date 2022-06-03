4a49db8d4f8f7b6ffd9f0dce8b71002fce460b34
nirlaksh <nirlaksh@cisco.com>
2022-06-03 10:12:39 +0530

    local2 file from local2 branch

---
+++ local2.py
@@ -0,0 +1,19 @@
+#Return the element whose count is max in list
+
+from collections import Counter
+l1 = [3,3,3,2, 4, 4, 5]
+d1 = Counter(l1)
+print("dicer is ",d1)
+max = 0
+op = 0
+for key,value in d1.items():
+    if max < value:
+        max = value
+        op = key
+
+print("key is ",op)
+print("I am adding local changes")
+
+
+
+
